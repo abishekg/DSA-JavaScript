db8306e9a7fa321381a26157edcb96069eff5d5f
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-05 15:52:25 +0530

    Completed LinkedList and Midpoint

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -14,6 +14,147 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(record) {
+    this.head = new Node(record, this.head);
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+    // return this.getAt(0);
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+    return node;
+
+    // return this.getAt(this.size() - 1);
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+    // this.removeAt(0);
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+    // this.removeAt(this.size() - 1);
+  }
+
+  insertLast(data) {
+    let last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+    // this.insertAt(data, this.size() - 1);
+  }
+
+  getAt(index) {
+    let count = 0;
+    if (index <= this.size()) {
+      let node = this.head;
+      while (node) {
+        if (index === count) {
+          return node;
+        }
+        node = node.next;
+        count++;
+      }
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    if (this.getAt(index)) {
+      var previous = this.getAt(index - 1);
+      previous.next = previous.next.next;
+      return;
+    }
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    if (this.getAt(index)) {
+      var node = new Node(data);
+      var previous = this.getAt(index - 1);
+      node.next = previous.next;
+      previous.next = node;
+    } else {
+      this.insertLast(data);
+    }
+  }
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
+  }
+
 }
 
 module.exports = { Node, LinkedList };
